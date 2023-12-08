ff6a0c0e685897601427429624ff8617e8187ff0
pashsidorenko <pashsidorenko@yandex.ru>
2023-12-09 00:58:24 +0300

    offer error

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 //import 'normalize.css';
 import './index.html';
 import './styles/main.scss';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+var swiper = new Swiper(".offerSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  modules: [Navigation, Pagination],
+});
